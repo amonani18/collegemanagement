1e505c5b2e46a6e48e397de29c550c52ef38ea1d
amonani18 <165120116+amonani18@users.noreply.github.com>
2025-02-22 02:03:53 -0500

    json file

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
     try {
         console.log("Connecting to:", process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
